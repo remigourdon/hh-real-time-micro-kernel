cce0f5f3cb82321213c48cb170483ed096f18fe3
remigourdon <contact@remigourdon.io>
2014-02-12 15:47:39 +0100

    Add lists structures definitions

--- lists.h
+++ lists.h
@@ -2,3 +2,23 @@
  * @file lists.h
  * @brief List management system definition.
  */
+
+/**
+ * @brief Generic list item.
+ */
+typedef struct l_obj {
+    TCB             *pTask;     /**< Pointer to the task function */
+    uint            nTCnt;      /**< Tick counter */
+    msg             *pMessage;
+    struct l_obj    *pPrevious; /**< Pointer to previous object in list */
+    struct l_obj    *pNext;     /**< Pointer to next object in list */
+} listobj;
+
+/**
+ * @brief Generic list.
+ */
+typedef struct {
+    listobj         *pHead;     /**< Pointer to the head of the list */
+    listobj         *pTail;     /**< Pointer to the tail of the list */
+} list;
+
