320bee30d6fdde5ecf135fe496526367c5658f8f
remigourdon <contact@remigourdon.io>
2014-02-12 17:35:45 +0100

    Defiinition of insert_readyList

--- lists.c
+++ lists.c
@@ -2,3 +2,26 @@
  * @file lists.c
  * @brief List management system implementation.
  */
+
+exception insert_readyList(listobj* elmt) {
+    listobj* current = readyList->pHead;
+
+    if(readyList->pHead == readyList->pTail == null) { // If empty list
+        readyList->pHead = elmt;
+        readyList->pTail = elmt;
+        return OK;
+    }
+    while(current->pNext != null) { // Until we reach the end of the list
+        if(elmt->pTask->DeadLine < current->pTask->DeadLine) {
+            current->pPrevious->pNext = elmt;
+            elmt->pPrevious = current->pPrevious;
+            current->pPrevious = elmt;
+            elmt.pNext = current;
+            return OK;
+        }
+        current = current->pNext;
+    }
+    current->pNext = elmt;
+    elmt->pPrevious = current;
+    return OK;
+}
