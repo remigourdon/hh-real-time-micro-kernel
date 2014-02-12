67df911701a1a41eb0acd817503e24b15733edbb
remigourdon <contact@remigourdon.io>
2014-02-12 16:46:18 +0100

    Add the list managing functions definitions.

--- lists.h
+++ lists.h
@@ -6,6 +6,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+
 /////////////////////
 // LIST STRUCTURES //
 /////////////////////
@@ -14,19 +15,80 @@
  * @brief Generic list item.
  */
 typedef struct l_obj {
-    TCB             *pTask;     /**< Pointer to the task function */
-    uint            nTCnt;      /**< Tick counter */
+    TCB             *pTask;     /**< Pointer to the task function. */
+    uint            nTCnt;      /**< Tick counter. */
     msg             *pMessage;
-    struct l_obj    *pPrevious; /**< Pointer to previous object in list */
-    struct l_obj    *pNext;     /**< Pointer to next object in list */
+    struct l_obj    *pPrevious; /**< Pointer to previous object in list. */
+    struct l_obj    *pNext;     /**< Pointer to next object in list. */
 } listobj;
 
 /**
  * @brief Generic list.
  */
 typedef struct {
-    listobj         *pHead;     /**< Pointer to the head of the list */
-    listobj         *pTail;     /**< Pointer to the tail of the list */
+    listobj         *pHead;     /**< Pointer to the head of the list. */
+    listobj         *pTail;     /**< Pointer to the tail of the list. */
 } list;
+
+
+///////////////////////////////
+// FUNCTIONS TO MANAGE LISTS //
+///////////////////////////////
+
+/**
+ * @brief Insert a new element in the readyList.
+ * 
+ * The list is sorted from the lowest deadline values first.
+ * 
+ * @param elmt Pointer to the element to be inserted.
+ */
+void insert_readyList(listobj* elmt);
+
+/**
+ * @brief Extract an element from the readyList.
+ *
+ * The first element of the list (lowest deadline) is extracted.
+ * 
+ * @return Pointer to the extracted element.
+ */
+listobj* extract_readyList(void);
+
+/**
+ * @brief Insert a new element in the timerList.
+ *
+ * The list is sorted from the lowest number of ticks values first.
+ * 
+ * @param elmt  Pointer to the element to be inserted.
+ * @param nTCnt Number of ticks to wait.
+ */
+void insert_timerList(listobj* elmt, uint nTCnt);
+
+/**
+ * @brief Extract an element from the timerList.
+ *
+ * The first element of the list (lowest tick counter) is extracted.
+ * 
+ * @return  Pointer to the extracted element.
+ */
+listobj* extract_timerList(void);
+
+/**
+ * @brief Insert a new element in the waitingList.
+ *
+ * The list is sorted from the lowest deadline values first.
+ * 
+ * @param elmt Pointer to the element to be inserted.
+ */
+void insert_waitingList(listobj* elmt);
+
+/**
+ * @brief Extract an element from the waitingList.
+ *
+ * The element corresponding to the parameter is extracted.
+ * 
+ * @param  elmt Pointer to the element to be extracted.
+ * @return      Pointer to the extracted element.
+ */
+listobj* extract_waitingList(listobj* elmt);
 
 #endif
