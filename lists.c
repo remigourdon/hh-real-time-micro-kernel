18ff117182e46c04d96ad5dab2a1762520ccb907
remigourdon <contact@remigourdon.io>
2014-02-12 19:44:56 +0100

    Definition of insert_waitingList

--- lists.c
+++ lists.c
@@ -64,3 +64,26 @@ listobj* extract_timerList(void) {
     firs->pNext = NULL;
     return first;
 }
+
+exception insert_waitingList(listobj* elmt) {
+    listobj* current = waitingList->pHead;
+
+    if(waitingList->pHead == waitingList->pTail == NULL) { // If empty list
+        waitingList->pHead = elmt;
+        waitingList->pTail = elmt;
+        return OK;
+    }
+    while(current->pNext != NULL) { // Until we reach the end of the list
+        if(elmt->pTask->DeadLine < current->pTask->DeadLine) {
+            current->pPrevious->pNext = elmt;
+            elmt->pPrevious = current->pPrevious;
+            current->pPrevious = elmt;
+            elmt.pNext = current;
+            return OK;
+        }
+        current = current->pNext;
+    }
+    current->pNext = elmt;
+    elmt->pPrevious = current;
+    return OK;
+}
