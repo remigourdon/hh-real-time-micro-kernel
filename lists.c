149e9477347ed02a7ee23a0c0e7ddd751d0b41a0
remigourdon <contact@remigourdon.io>
2014-02-19 16:56:09 +0100

    Implement create_emptyList

--- lists.c
+++ lists.c
@@ -2,3 +2,19 @@
  * @file lists.c
  * @brief List management system implementation.
  */
+
+list* create_emptyList(void) {
+    list* newList;
+    newList = (list*)malloc(sizeof(list));
+
+    newList->pHead = (listobj*)malloc(sizeof(listobj));
+    newList->pTail = (listobj*)malloc(sizeof(listobj));
+
+    newList->pHead->pPrevious = NULL;
+    newList->pHead->pNext     = newList->pTail;
+
+    newList->pTail->pNext     = NULL;
+    newList->pTail->pPrevious = newList->pHead;
+
+    return newList;
+}
