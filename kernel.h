774cc116f9727ee3e4d3377fcba3be3eac0515f3
remigourdon <contact@remigourdon.io>
2014-02-24 16:50:09 +0100

    Create task, message and mailbox structures

--- kernel.h
+++ kernel.h
@@ -13,7 +13,7 @@
 #include <stdlib.h>
 #include "lists.h"
 
-#ifndef texas_dsp
+#ifdef texas_dsp
 
 #define CONTEXT_SIZE    34-2
 
@@ -44,5 +44,50 @@ typedef int             exception;
 typedef int             bool;
 typedef unsigned int    uint;
 typedef int             action;
+
+#ifdef texas_dsp
+typedef struct {
+    void    (*PC)();
+    uint    *SP;
+    uint    Context[CONTEXT_SIZE];
+    uint    StackSeg[STACK_SIZE];
+    uint    DeadLine;
+} TCB;
+#else
+/**
+ * @brief Task Control Block (TCB)
+ */
+typedef struct {
+    void    (*PC)();                /**< Program Counter */
+    uint    *SP;                    /**< Stack Pointer */
+    uint    Context[CONTEXT_SIZE];  /**< Context placeholder */
+    uint    StackSeg[STACK_SIZE];   /**< Stack segment */
+    uint    DeadLine;
+    uint    SPSR;
+} TCB;
+#endif
+
+/**
+ * @brief Message item
+ */
+typedef struct msgobj {
+    char            *pData;         /**< Pointer to message data */
+    exception       Status;         /**< Message status */
+    struct l_obj    *pBlock;        /**< Pointer to blocking item */
+    struct msgobj   *pPrevious;     /**< Pointer to previous msg in mailbox */
+    struct msgobj   *pNext;         /**< Pointer to next msg in mailbox */
+} msg;
+
+/**
+ * @brief Mailbox structure
+ */
+typedef struct {
+    msg             *pHead;         /**< Pointer to the head of the mailbox */
+    msg             *pTail;         /**< Pointer to the tail of the mailbox */
+    int             nDataSize;      /**< Data size in bytes */
+    int             nMaxMessages;   /**< Maximum number of msgs */
+    int             nMessages;      /**< Current number of msgs */
+    int             nBlockedMsg;    /**< Current number of blocking msgs */
+} mailbox;
 
 #endif
