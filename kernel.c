dab0e12db4dc736187a6f23bcba260a679e0d352
remigourdon <contact@remigourdon.io>
2014-02-25 17:16:51 +0100

    Implement init_kernel

--- kernel.c
+++ kernel.c
@@ -3,3 +3,24 @@
  * @brief Kernel declarations
  */
 
+#include "kernel.h"
+#include "lists.h"
+
+exception init_kernel(void) {
+    set_ticks(0);   // Set tick counter to zero
+
+    // Create necessary data structures
+    readyList = create_emptyList();
+    timingList = create_emptyList();
+    waitingList = create_emptyList();
+
+    // Create an idle task
+    create_task(IDLE, UINT_MAX);
+
+    // Set the kernel in startup mode
+    Running = task_IDLE;
+    LoadContext();
+
+    // Return status
+    return OK;
+}
