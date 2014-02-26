84d4d446e5f5eb6b377355bba7ba13158b527043
remigourdon <contact@remigourdon.io>
2014-02-26 15:47:43 +0100

    Modify create_task to create the listobj elmt to be inserted in the readyList

--- kernel.c
+++ kernel.c
@@ -29,26 +29,32 @@ exception create_task(void (*body)(), uint deadline) {
     // The variable is saved in the stack of the current running task (volatile)
     volatile int first = TRUE;
 
-    // Allocate memory for TCB
-    TCB* newTask;
+    listobj* newElmt;
 
     // BEGIN CRITICAL ZONE
     isr_off();
-    newTask = (TCB*)malloc(sizeof(TCB));
+    newElmt         = (listobj*)malloc(sizeof(listobj));
+    newElmt->pTask  = (TCB*)malloc(sizeof(TCB));
     isr_on();
     // END CRITICAL ZONE
 
+    // Initialize listobj attributes
+    newElmt->nTCnt      = 0;
+    newElmt->pMessage   = NULL;
+    newElmt->pPrevious  = NULL;
+    newElmt->pNext      = NULL;
+
     // Set deadline in TCB
-    newTask->DeadLine = deadline;
+    newElmt->pTask->DeadLine = deadline;
 
     // Set TCB's PC to point to the task body
-    newTask->PC = body;
+    newElmt->pTask->PC = body;
 
     // Set TCB's SP to point to the task segment
-    newTask->SP = &(newTask->StackSeg[STACK_SIZE-1]);
+    newElmt->pTask->SP = &(newElmt->pTask->StackSeg[STACK_SIZE-1]);
 
     if(MODE == INIT) {      // If startup mode
-        insert_readyList(newTask);
+        insert_readyList(newElmt);
         return OK;
     }
     else {
@@ -56,10 +62,12 @@ exception create_task(void (*body)(), uint deadline) {
         SaveContext();
         if(first == TRUE) {     // If first execution
             first = FALSE;
-            insert_readyList(newTask);
+            insert_readyList(newElmt);
             LoadContext();
         }
     }
     return OK;
 }
+
+
 
