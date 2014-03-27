6ca10c4413598dd4389d1a42568beef20a944683
remigourdon <contact@remigourdon.io>
2014-03-27 15:35:23 +0100

    Add freeing code for lists in the main function

--- main.c
+++ main.c
@@ -7,5 +7,20 @@
 
 int main(void) {
     init_kernel();
+
+    // Free memory allocated for the lists
+    // Will not be reached normally
+    free(readyList->pHead);
+    free(readyList->pTail);
+    free(readyList);
+
+    free(timerList->pHead);
+    free(timerList->pTail);
+    free(timerList);
+
+    free(waitingList->pHead);
+    free(waitingList->pTail);
+    free(waitingList);
+
     return 0;
 }
